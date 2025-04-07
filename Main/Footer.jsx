8b1eb2be3585be2ec9566ca6da708dce5b9daa0b
Mateus de Lima Pinheiro <mateusp272@gmail.com>
2025-04-07 20:24:07 -0300

    Projeto evoluido

--- Main/Footer.jsx
+++ Main/Footer.jsx
@@ -5,7 +5,7 @@ const Footer = () => {
 
     return(
         <footer className="bg-dark text-white text-center p-3 footer">
-            <p>Restaurante React Gourmet {new Date().getFullYear()}</p>
+            <p>Restaurante React Gourmet Trabalho {new Date().getFullYear()}</p>
         </footer>
     )
 }
